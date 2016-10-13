5235d71029156db68107f0bd0ac647d1dae9bedb
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-10-13 19:12:15 +0200

    initial File with the header Comment nothing else not executable

---
+++ statisticalmethods.py
@@ -0,0 +1,14 @@
+###############################################################################
+# Statisticalmethods.py
+# Description:  This Module does some simple staistics on the results of the
+#               Praktikum P1. Intended to simplify the process of statistical
+#               analysis of the aquired data.
+# Author:       Alexander Becker
+#               David Schilling
+# Created:      12.10.2016
+###############################################################################
+
+# imports
+import csv
+import numpy
+
