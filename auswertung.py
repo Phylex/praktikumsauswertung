04dac9101a0d5e827b2408f6ba0fbc556595874c
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-11-05 15:14:59 +0100

    Added a easy interface to the kafe package for fitting funktions to datasets. It works

--- auswertung.py
+++ auswertung.py
@@ -11,6 +11,7 @@ import dateutil.parser as dateparser
 import time
 import csv
 import kafe
+from kafe import function_library
 #================================= Statical Stuff =============================
 einheitenpraefixe = {-15:'f',-12:'p',-9:'n',-6:'micro',-5:'10^-5',-3:'m',-2:'c',-1:'d',0:'',2:'h',3:'k',6:'M',9:'G',12:'T',15:'Ex'}
 
@@ -207,11 +208,25 @@ def import_experiment_data(filepath):
 #------------------------------------------------------------------------------
 # This funktion prepares the data given by the messen.py program
 # we will have the job of Createing a Dataset and then returning it
-def transform_data_into_kafe_Dataset_linear_regression(messgroessen, messungen):
+def transform_data_into_kafe_Dataset_linear_regression(messgroessen, messungen, experiment_Name):
     if len(messgroessen) > 2 or len(messgroessen) > 2:
         return None
     else:
-        kafedata = kafe.Dataset(data=(elem for elem in messungen),axis_lables=[messgroesse[0] for messgroesse in messgroessen],axis_units=[ einheitenpraefixe[messgroesse[3]]+messgroesse[1] for messgroesse in messgroessen])
+        kafedata = kafe.dataset.Dataset(data=[elem for elem in messungen], axis_lables=[messgroesse[0] for messgroesse in messgroessen], axis_units=[ einheitenpraefixe[messgroesse[3]]+messgroesse[1] for messgroesse in messgroessen],basename = experiment_Name)
+        for i, elem in enumerate(messgroessen):
+            kafedata.add_error_source(i,'simple', elem[-1])
+        return kafedata
+
+#------------------------------------------------------------------------------
+# This Funktion will generate a fit object from the dataset built by the fun-
+# ction above the fitting funktion defaults to a 2 parameter linear fit
+def build_kafe_fit_and_plot_object(kafedataset,fitfunktion=kafe.function_library.linear_2par):
+    kafefit = kafe.Fit(kafedataset,fitfunktion)
+    kafefit.do_fit()
+    kafeplot = kafe.Plot(kafefit)
+    kafeplot.plot_all()
+    return [kafefit,kafeplot]
+
 ################################## Main Code ##################################
 # this can also be used as a module so we have to put the main code in here
 if __name__ == "__main__":
